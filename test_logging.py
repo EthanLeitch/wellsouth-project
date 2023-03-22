b3bab0111782efdb5946d38e237d89a3ec203487
Ethan <ethanjohnleitch@gmail.com>
2023-03-23 12:32:14 +1300

    Added basic email functionality

--- test_logging.py
+++ test_logging.py
@@ -1,6 +1,16 @@
+# General modules
 import logging
 from os import environ
+
+# Internet modules
 import requests
+import yagmail
+
+# Security-related modules
+from dotenv import dotenv_values
+
+# Load .env values
+config = dotenv_values(".env")
 
 # Conform to ISO 8601
 FORMAT = '%(asctime)s %(message)s'
@@ -8,8 +18,14 @@ logging.basicConfig(format=FORMAT)
 logger = logging.getLogger('tcpserver')
 logger.info(f"Program started via {environ['SHELL']}")
 
+yag = yagmail.SMTP(config["EMAIL_ADDRESS"], config["EMAIL_PASSWORD"])
+contents = [
+    "Some random content...",
+]
+yag.send(config["RECEIVING_ADDRESS"], 'Test Email', contents)
+
+"""
 url = "https://api.bamboohr.com/api/gateway.php/companyDomain/v1/employees/changed"
-
 response = requests.get(url)
-
 print(response.text)
+"""
