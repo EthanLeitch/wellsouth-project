89938b2bfaac4726aff41bcadc66bd085943410a
Ethan <ethanjohnleitch@gmail.com>
2023-03-24 10:42:40 +1300

    Added logging prototype to email

--- test_logging.py
+++ test_logging.py
@@ -1,6 +1,7 @@
 # General modules
 import logging
 from os import environ
+import time
 
 # Internet modules
 import requests
@@ -14,15 +15,24 @@ config = dotenv_values(".env")
 
 # Conform to ISO 8601
 FORMAT = '%(asctime)s %(message)s'
-logging.basicConfig(format=FORMAT)
-logger = logging.getLogger('tcpserver')
+logging.basicConfig(filename="log.txt", format=FORMAT, level=logging.INFO)
+logger = logging.getLogger()
 logger.info(f"Program started via {environ['SHELL']}")
 
-yag = yagmail.SMTP(config["EMAIL_ADDRESS"], config["EMAIL_PASSWORD"])
-contents = [
-    "Some random content...",
-]
-yag.send(config["RECEIVING_ADDRESS"], 'Test Email', contents)
+def critical_error():
+    logger.warning("Something went wrong! Sending email message...")
+
+    yag = yagmail.SMTP(config["EMAIL_ADDRESS"], config["EMAIL_PASSWORD"])
+    contents = [
+        "Something went wrong! The Python program has been automatically halted.",
+        "Below this message is the program's log (log.txt). It may help with diagnosing the problem."
+        " ",
+    ]
+    yag.send(config["RECEIVING_ADDRESS"], f'Python error @ {time.asctime()}', contents, attachments="log.txt")
+
+    exit()
+
+# critical_error()
 
 """
 url = "https://api.bamboohr.com/api/gateway.php/companyDomain/v1/employees/changed"
